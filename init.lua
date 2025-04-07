2009ed1ddc8f4d96e3aca78471704bfc672d7ee2
Don <serfdon@gmail.com>
2025-04-07 05:21:49 -0700

    Removed homedecor

--- init.lua
+++ init.lua
@@ -4,8 +4,8 @@ dofile(minetest.get_modpath("myroofs").."/straw.lua")
 dofile(minetest.get_modpath("myroofs").."/blocks.lua")
 dofile(minetest.get_modpath("myroofs").."/chimney.lua")
 
-if minetest.get_modpath("homedecor") then
-	dofile(minetest.get_modpath("myroofs").."/homedecor.lua")
-		return
-	end
+--if minetest.get_modpath("homedecor") then
+--	dofile(minetest.get_modpath("myroofs").."/homedecor.lua")
+--		return
+--	end
 
